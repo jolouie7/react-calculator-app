065d6cd72fccc99b14dff4415b630fea061a6aeb
Your_Name <joseph161216@gmail.com>
2021-04-21 15:11:48 -0700

    Add ability to clear all and delete 1 from current expression

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+
 import Calculator from './components/Calculator';
 
 function App() {
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,30 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Calculator = () => {
+  const [prevOperand, setPrevOperand] = useState("123+123")
+  const [currentOperand, setCurrentOperand] = useState("456");
+  // const [endingOperand, setEndingOperand] = useState("");
+
+  const handleClick = (e) => {
+    console.log(e.target.value)
+    const currVal = (e.target.value).toString()
+    setCurrentOperand(currentOperand + currVal)
+  }
+
+  const handleAllClear = () => {
+    setCurrentOperand("")
+  }
+
+  const handleDelete = () => {
+    const expression = currentOperand.slice(0, currentOperand.length-1)
+    console.log(expression)
+    setCurrentOperand(expression);
+  }
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="prev-operand">123 + 123</div>
-        <div className="current-operand">456</div>
+        <div data-prev-operand className="prev-operand">{prevOperand}</div>
+        <div data-current-operand className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
-      <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button className="span-two">=</button>
+      <button onClick={handleAllClear} data-all-clear className="span-two">AC</button>
+      <button onClick={handleDelete} data-delete>DEL</button>
+      <button onClick={handleClick} value={"÷"} data-operation>÷</button>
+      <button onClick={handleClick} value={1} data-number>1</button>
+      <button onClick={handleClick} value={2} data-number>2</button>
+      <button onClick={handleClick} value={3} data-number>3</button>
+      <button onClick={handleClick} value={"*"} data-operation>*</button>
+      <button onClick={handleClick} value={4} data-number>4</button>
+      <button onClick={handleClick} value={5} data-number>5</button>
+      <button onClick={handleClick} value={6} data-number>6</button>
+      <button onClick={handleClick} value={"+"} data-operation>+</button>
+      <button onClick={handleClick} value={7} data-number>7</button>
+      <button onClick={handleClick} value={8} data-number>8</button>
+      <button onClick={handleClick} value={9} data-number>9</button>
+      <button onClick={handleClick} value={"-"} data-operation>-</button>
+      <button onClick={handleClick} value={"."} data-number>.</button>
+      <button onClick={handleClick} value={0} data-number>0</button>
+      <button data-equals className="span-two">=</button>
     </div>
   );
 }
